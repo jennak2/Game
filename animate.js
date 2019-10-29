4d7209e9f48328faa5ac1fe11a93b4127fce7454
Alex Saffari <asaffari1@hwemail.com>
2019-10-29 12:01:38 -0700

    Maze is made and the blood drops are distributed
    the code is horrendous but it works time is of the essence

--- animate.js
+++ animate.js
@@ -11,8 +11,45 @@ function runGame() {
     // 2 - Clear the CANVAS
     context.clearRect(0, 0, 600, 300);
 
+    // 3 draw the blood drops
+      context.beginPath();
+      context.lineWidth = "6";
+      context.strokeStyle = "red";
+      context.rect(COORD.x1, COORD.y1, 5, 5);
+      context.stroke();
+
+      context.beginPath();
+      context.lineWidth = "6";
+      context.strokeStyle = "red";
+      context.rect(COORD.x2, COORD.y2, 5, 5);
+      context.stroke();
+
+      context.beginPath();
+      context.lineWidth = "6";
+      context.strokeStyle = "red";
+      context.rect(COORD.x3, COORD.y3, 5, 5);
+      context.stroke();
+
+      context.beginPath();
+      context.lineWidth = "6";
+      context.strokeStyle = "red";
+      context.rect(COORD.x4, COORD.y4, 5, 5);
+      context.stroke();
+
+      context.beginPath();
+      context.lineWidth = "6";
+      context.strokeStyle = "red";
+      context.rect(COORD.x5, COORD.y5, 5, 5);
+      context.stroke();
+
+      context.beginPath();
+      context.lineWidth = "6";
+      context.strokeStyle = "red";
+      context.rect(COORD.x6, COORD.y6, 5, 5);
+      context.stroke();
+
     // 3 - Draw new items
-    
+
 
   } else {
     context.font = "30px Arial";
--- config.js
+++ config.js
@@ -10,3 +10,18 @@ var GAME = {
 var BLOOD = {
   collectedDrops : 0
 };
+
+var COORD = {
+  x1 : Math.random()*600,
+  y1 : Math.random()*300,
+  x2 : Math.random()*600,
+  y2 : Math.random()*300,
+  x3 : Math.random()*600,
+  y3 : Math.random()*300,
+  x4 : Math.random()*600,
+  y4 : Math.random()*300,
+  x5 : Math.random()*600,
+  y5 : Math.random()*300,
+  x6 : Math.random()*600,
+  y6 : Math.random()*300
+};
